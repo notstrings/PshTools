e898dc1c6170f7c88edd4c103b61fc59a63a68bc
notstrings@gmail.com <Honntouha1@>
2024-12-19 19:18:00 +0900

    aaa

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
 ﻿. "$($PSScriptRoot)/ModuleMisc.ps1"
 
 Add-Type -Assembly System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show("ここにメッセージ", $args -join " ")
+[System.Windows.Forms.MessageBox]::Show($args -join " ", "title")
