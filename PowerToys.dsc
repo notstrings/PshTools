d1f6fdc6c5292858fe9b2a8e9c45b22a8c390f26
satou <yoshikazu-satou@srze.co.jp>
2024-12-20 17:15:26 +0900

    aaa

---
+++ PowerToys.dsc
@@ -0,0 +1,21 @@
+﻿properties:
+  resources:
+    - resource: Microsoft.WinGet.DSC/WinGetPackage
+      id: installPowerToys
+      directives:
+        description: Install PowerToys
+        allowPrerelease: true
+      settings:
+        id: Microsoft.PowerToys
+        source: winget
+    - resource: Microsoft.PowerToys.Configure/PowerToysConfigure
+      dependsOn:
+        - installPowerToys
+      directives:
+        description: Configure PowerToys
+      settings:
+        Awake:
+          Enabled: true
+          KeepDisplayOn: true
+          Mode: INDEFINITE
+  configurationVersion: 0.2.0
---
+++ ScheduleShutdown.ps1
@@ -0,0 +1,37 @@
+﻿$ErrorActionPreference = "Continue"
+
+function local:ExecWinget([string] $PackageID) {
+    Write-Host "winget install --id $PackageID -e --accept-package-agreements --source winget"
+    winget install --id $PackageID -e --accept-package-agreements --source winget
+    Write-Host ""
+}
+
+switch (
+    $Host.UI.PromptForChoice(
+        "シャットダウン予約",
+        "`tシャットダウン予定時間を選択してください",
+        @("Exit(&E)", "1時間(&1)", "3時間(&3)", "5時間(&5)", "シャットダウン取消(&C)"),
+        0
+    )
+) {
+    0 { exit 0               }
+    1 { shutdown -s -t  3600 }
+    2 { shutdown -s -t 10800 }
+    3 { shutdown -s -t 18000 }
+    4 { shutdown -a          }
+}
+
+switch (
+    $Host.UI.PromptForChoice(
+        "画面ロック抑止",
+        "`t画面ロック中はシャットダウンされません`n`t画面ロック回避ツールをインストールしますか？",
+        @("NO(&N)","YES(&Y)"),
+        0
+    )
+) {
+    0 { exit 0 }
+    1 {
+        Write-Host "インストールが完了したらAwakeツールを有効にしてください"
+        winget configure .\PowerToys.dsc
+    }
+}
