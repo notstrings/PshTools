92c958e4d635be83d1163e6d005a5989d4e9e626
satou <yoshikazu-satou@srze.co.jp>
2025-01-07 12:15:30 +0900

    シャットダウン予約コメントを拡充

--- ScheduleShutdown.ps1
+++ ScheduleShutdown.ps1
@@ -1,5 +1,11 @@
 ﻿$ErrorActionPreference = "Continue"
 
+# ユーザ表示名取得
+$udm = $env:USERDOMAIN
+$uid = $env:USERNAME
+$fullname = ([adsi]"WinNT://$udm/$uid,user").fullname
+
+# 予約時間選択
 switch (
     $Host.UI.PromptForChoice(
         "シャットダウン予約",
@@ -9,10 +15,10 @@ switch (
     )
 ) {
     0 { exit 0 }
-    1 { shutdown /s /t  3600 /c "ユーザーによるシャットダウン予約" /f /d p:0:0 }
-    2 { shutdown /s /t 10800 /c "ユーザーによるシャットダウン予約" /f /d p:0:0 }
-    3 { shutdown /s /t 18000 /c "ユーザーによるシャットダウン予約" /f /d p:0:0 }
-    3 { shutdown /s /t 18000 /c "ユーザーによるシャットダウン予約" /f /d p:0:0 }
-    3 { shutdown /s /t 28800 /c "ユーザーによるシャットダウン予約" /f /d p:0:0 }
+    1 { shutdown /s /t  3600 /c "$($fullname)によるシャットダウン予約" /f /d p:0:0 }
+    2 { shutdown /s /t 10800 /c "$($fullname)によるシャットダウン予約" /f /d p:0:0 }
+    3 { shutdown /s /t 18000 /c "$($fullname)によるシャットダウン予約" /f /d p:0:0 }
+    3 { shutdown /s /t 18000 /c "$($fullname)によるシャットダウン予約" /f /d p:0:0 }
+    3 { shutdown /s /t 28800 /c "$($fullname)によるシャットダウン予約" /f /d p:0:0 }
     4 { shutdown /a }
 }
