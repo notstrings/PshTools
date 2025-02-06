556fd83d49bbb7d8084f7cd9d22313c4ac403a51
satou <yoshikazu-satou@srze.co.jp>
2025-02-06 10:21:42 +0900

    スクリプトブロック直指定する方が楽だと判明

--- ModuleMisc.ps1
+++ ModuleMisc.ps1
@@ -1355,38 +1355,30 @@ function ShowToast {
     )
     begin {}
     process {
-        $blk = {
-            function ShowToastInner($Title, $Message, $Detail) {
-                [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
-                [Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
-                [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
-                $app_id = "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe"
-                $content = ""
-                $content += "<?xml version='1.0' encoding='utf-8'?>"
-                $content += "<toast>"
-                $content += "  <visual>"
-                $content += "    <binding template='ToastGeneric'>"
-                $content += "        <text>$Title</text>"
-                $content += "        <text>$Message</text>"
-                $content += "        <text>$Detail</text>"
-                $content += "    </binding>"
-                $content += "  </visual>"
-                $content += "</toast>"
-                $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
-                $xml.LoadXml($content)
-                $toast = New-Object Windows.UI.Notifications.ToastNotification $xml
-                [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($app_id).Show($toast)
-            }
-            ShowToastInner "__ARG1" "__ARG2" "__ARG3"
-        }
-        $enctxt = $blk.ToString()
-        $enctxt = $enctxt.Replace("__ARG1", $Title)
-        $enctxt = $enctxt.Replace("__ARG2", $Message)
-        $enctxt = $enctxt.Replace("__ARG3", $Detail)
-        $enccmd = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($enctxt))
-        Start-Process -NoNewWindow "powershell.exe" -ArgumentList "-NoProfile -EncodedCommand $enccmd"
+        $null = powershell {
+            param ($Title, $Message, $Detail)
+            [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+            [Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+            [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
+            $app_id = "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe"
+            $content = ""
+            $content += "<?xml version='1.0' encoding='utf-8'?>"
+            $content += "<toast>"
+            $content += "  <visual>"
+            $content += "    <binding template='ToastGeneric'>"
+            $content += "        <text>$Title</text>"
+            $content += "        <text>$Message</text>"
+            $content += "        <text>$Detail</text>"
+            $content += "    </binding>"
+            $content += "  </visual>"
+            $content += "</toast>"
+            $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+            $xml.LoadXml($content)
+            $toast = New-Object Windows.UI.Notifications.ToastNotification $xml
+            [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($app_id).Show($toast)
+        } -args $Title, $Message, $Detail
     }
-end {}
+    end {}
 }
 
 <#
